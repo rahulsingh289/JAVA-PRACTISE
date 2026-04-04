1ca503c22ae761519e683a3c94ef7498c9bf2f87
Rahul Singh <rahulsinghchand28@gmail.com>
2026-04-04 21:56:10 +0530

    added code

---
+++ BasicSynchronization.java
@@ -0,0 +1,42 @@
+
+
+
+class Counter {
+    int count = 0;
+
+    // synchronized method
+    public synchronized void increment() {
+        count++;
+    }
+}
+
+class MyThread extends Thread {
+    Counter c;
+
+    MyThread(Counter c) {
+        this.c = c;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            c.increment();
+        }
+    }
+}
+
+public class BasicSynchronization  {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
+
+        MyThread t1 = new MyThread(c);
+        MyThread t2 = new MyThread(c);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Count: " + c.count);
+    }
+}
